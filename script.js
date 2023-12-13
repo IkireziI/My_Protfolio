902b370980d1c133ac0ac6c62e2b00f78d0acd18
IkireziI <i.ikirezi@alustudent.com>
2023-12-13 10:26:11 +0200

    second commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// function scrollToSection(sectionId) {
+//     const section = document.getElementById(sectionId);
+//     if (section) {
+//       window.scrollTo({
+//         top: section.offsetTop,
+//         behavior: 'smooth'
+//       });
+//     }
+//   }
+
+//   // Form submission handling
+//   function submitForm(event) {
+//     event.preventDefault();
+//     // Add your form submission logic here
+//     // For example, you can use AJAX to send the form data to a server
+//     console.log('Form submitted!');
+//   }
